423dafd75c5d02fcb20e650e7c806548a145ae39
Paweł Pasternak <naczekp@gmail.com>
2023-03-23 15:00:08 +0100

    added image quality,  variable names changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const image = document.querySelector("#image")
-const a_download = document.querySelector("#download")
+const imageFile = document.querySelector("#image")
+const downloadLink = document.querySelector("#download")
 const drop_area = document.querySelector(".drop_area")
 const uploaded_image_div = document.querySelector("#uploaded_image_div")
 const resize_width = document.querySelector("#resize_width")
@@ -8,10 +8,9 @@ const quality = document.querySelector('#quality')
 const resized_image = document.querySelector("#resized_image")
 const size = document.querySelectorAll('.size')
 const canvas = document.querySelector('#canvas')
-const resized_link = document.querySelector('#resized_link')
+const resizedLink = document.querySelector('#resized_link')
 
 const img = new Image()
-let img_resized = new Image()
 const ctx = canvas.getContext('2d')
 
 function updateImageSizeValues(width,height) {
@@ -24,25 +23,29 @@ function updateCanvasAndUrl(image, quality_flag=false) {
  
     canvas.width = parseInt(resize_width.value)
     canvas.height = parseInt(resize_height.value)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(image, 0, 0, parseInt(resize_width.value), parseInt(resize_height.value))
-    const imageQuality = parseInt(quality.value) / 100
     
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    
+    ctx.drawImage(image, 0, 0, parseInt(resize_width.value), parseInt(resize_height.value))
+   
+    const imageQuality = parseInt(quality.value) / 100
+
     canvas.toBlob(function(blob) {
-        a_download.href = URL.createObjectURL(blob) 
-        resized_link.style.display = 'inline'
-        // if(quality_flag) {
-        //     img_resized.src = URL.createObjectURL(blob)
-        //     updateCanvasAndUrl(img_resized, quality_flag = false)
-        // }
+        downloadLink.href = URL.createObjectURL(blob)
+        resizedLink.style.display = 'inline'
+        if(quality_flag){
+            const img_r = new Image()
+            img_r.src = URL.createObjectURL(blob)
+            img_r.onload = function() {
+                updateCanvasAndUrl(img_r, false)
+            }
+        }
         }, "image/jpeg", imageQuality)
 }
 
-image.addEventListener('change', (e) => {
+imageFile.addEventListener('change', (e) => {
     img.onload = function() {
         updateImageSizeValues(img.width, img.height)
-        // img_resized.src = img.src
-        // resized_image.appendChild(img_resized)
         updateCanvasAndUrl(img)
     }
     img.src = URL.createObjectURL(e.target.files[0])
@@ -61,7 +64,7 @@ resize_height.addEventListener('input', (e) => {
 
 quality.addEventListener('input', (e) => {
     if(!e.target.value) return
-    updateCanvasAndUrl(img, quality_flag=true)
+    updateCanvasAndUrl(img, true)
 })
 
 drop_area.addEventListener('dragover', function(e) {
